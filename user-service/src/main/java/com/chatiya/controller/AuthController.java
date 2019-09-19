a2b0ebb852d775461992ddbcb3043c0ccb117e52
Archit Sud <archit@Archits-Mac.local>
2019-09-19 15:26:31 +0530

    feat(API Gateway): Added API gateway for routing

--- user-service/src/main/java/com/chatiya/controller/AuthController.java
+++ user-service/src/main/java/com/chatiya/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/users/auth")
 public class AuthController extends BaseController{
 
     private UserRepository userRepository;
